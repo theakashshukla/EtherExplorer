848b8561895c0f01315f5c96327f97eaebb1d675
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-02 16:24:48 +0530

    Added Navigation and Packages

---
+++ config/site.ts
@@ -0,0 +1,25 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "BlockPrice",
+  description:"Quickly check the price of your favorite crypto currency",
+  mainNav: [
+    // {
+    //   title: "Home",
+    //   href: "/",
+    // },
+    // {
+    //   title: "About",
+    //   href: "/about",
+    // },
+    // {
+    //   title: "Contact",
+    //   href: "/contact",
+    // },
+  ],
+  links: {
+    twitter: "https://twitter.com/theskaai",
+    github: "https://github.com/theakashshukla",
+    docs: "#",
+  },
+};
