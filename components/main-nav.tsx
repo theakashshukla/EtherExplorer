7077b2ef68625befd206191c86c46b54f2de274a
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-02 18:11:36 +0530

    Update

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,19 +1,18 @@
-import * as React from "react"
-import Link from "next/link"
+import * as React from "react";
+import Link from "next/link";
 
-import { NavItem } from "@/types/nav"
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
+import { NavItem } from "@/types/nav";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 
-import { Icons } from "@/components/icons"
+import { Icons } from "@/components/icons";
 
 interface MainNavProps {
-  items?: NavItem[]
+  items?: NavItem[];
 }
 
 export function MainNav({ items }: MainNavProps) {
   return (
-
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.verify className="h-6 w-6" />
@@ -36,9 +35,8 @@ export function MainNav({ items }: MainNavProps) {
                 </Link>
               )
           )}
-     
         </nav>
       ) : null}
     </div>
-  )
+  );
 }
