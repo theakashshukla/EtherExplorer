9de20a4d978730acb2e290dd5760cad877140e50
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-02 16:21:13 +0530

    Add Home component with Ether title

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center m-2">
+      <h1 className="text-4xl font-bold">Ether</h1>
+      
+    </main>
+  );
+}
