2099f2897c1ba245098dd3746948101281d9e6ed
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-05 01:36:32 +0530

    Added error handling for API requests and implemented functions to fetch Ether balance, normal transactions, ERC20 token transfers, and Ethereum price

--- app/api/etherscan.ts
+++ app/api/etherscan.ts
@@ -1,82 +1,71 @@
-import axios from 'axios';
-import { NormalTransaction, ERC20TokenTransfer, EtherPriceResponse, GasPriceResponse } from '@/types/ether';
+import axios, { AxiosInstance, AxiosError  } from 'axios';
 
-// add ether scan api key form env
 const ETHERSCAN_API_KEY = process.env.NEXT_PUBLIC_ETHERSCAN_KEY;
 
-if(!ETHERSCAN_API_KEY) {
+if (!ETHERSCAN_API_KEY) {
   throw new Error('Missing EtherScan Api key from env variable');
 }
 
-const etherscanBaseURL = 'https://api.etherscan.io/api'; // Etherscan API base URL
+const etherscanBaseURL = 'https://api.etherscan.io/api';
 
+const etherscan: AxiosInstance = axios.create({
+  baseURL: etherscanBaseURL,
+  params: {
+    apikey: ETHERSCAN_API_KEY,
+  },
+});
 
-
-// Function to get the ether balance of an address
-export const getEtherBalance = async (address: string): Promise<string> => {
+export const makeEtherscanRequest = async (
+  module: string,
+  action: string,
+  params: Record<string, string | number> = {}
+) => {
   try {
-    const response = await axios.get(`${etherscanBaseURL}?module=account&action=balance&address=${address}&apikey=${ETHERSCAN_API_KEY}`);
+    const response = await etherscan.get('', {
+      params: {
+        module,
+        action,
+        ...params,
+      },
+    });
     return response.data.result;
   } catch (error) {
-    console.error('Error fetching ether balance:', error);
-    throw error;
+    const axiosError = error as AxiosError | undefined;
+    const errorMessage = axiosError?.message || 'Unknown error';
+    console.error(`Failed to fetch ${module} details: ${errorMessage}`);
+    throw new Error(`Failed to fetch ${module} details: ${errorMessage}`);
   }
 };
 
+export const getEtherBalance = async (address: string) => {
+  return makeEtherscanRequest('account', 'balance', { address });
+};
 
-// Function to get the list of normal transactions for an address with pagination
 export const getNormalTransactions = async (
   address: string,
   page: number = 1,
   pageSize: number = 10
-): Promise<NormalTransaction[]> => {
-  try {
-    const response = await axios.get<{ result: NormalTransaction[] }>(
-      `${etherscanBaseURL}?module=account&action=txlist&address=${address}&page=${page}&offset=${pageSize}&apikey=${ETHERSCAN_API_KEY}`
-    );
-    return response.data.result;
-  } catch (error) {
-    console.error('Error fetching normal transactions:', error);
-    throw error;
-  }
+) => {
+  return makeEtherscanRequest('account', 'txlist', {
+    address,
+    page,
+    offset: pageSize,
+  });
+};
+
+export const getERC20TokenTransfers = async (address: string, pageSize: number = 10) => {
+  return makeEtherscanRequest('account', 'tokentx', {
+    address,
+    page: 1,
+    offset: pageSize,
+  });
 };
 
 
-// Function to get the list of ERC-20 token transfers for an address
-export const getERC20TokenTransfers = async (address: string): Promise<ERC20TokenTransfer[]> => {
-  try {
-    const response = await axios.get<{ result: ERC20TokenTransfer[] }>(
-      `${etherscanBaseURL}?module=account&action=tokentx&address=${address}&page=1&apikey=${ETHERSCAN_API_KEY}`
-    );
-    return response.data.result;
-  } catch (error) {
-    console.error('Error fetching ERC-20 token transfers:', error);
-    throw error;
-  }
+export const getEthPrice = async () => {
+  return makeEtherscanRequest('stats', 'ethprice');
 };
 
-// Function to fetch ETH price
-export const getEthPrice = async (): Promise<EtherPriceResponse> => {
-  try {
-    const response = await axios.get<{ result: EtherPriceResponse}>(
-      `${etherscanBaseURL}?module=stats&action=ethprice&apikey=${ETHERSCAN_API_KEY}`
-    );
-    return response.data.result;
-  } catch (error) {
-    console.error('Error fetching ETH price:', error);
-    throw error;
-  }
-};
-
-// Function to fetch Gas price
-export const getGasPrice = async (): Promise<GasPriceResponse> => {
-  try {
-    const response = await axios.get<{result: GasPriceResponse}>(
-      `${etherscanBaseURL}?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_API_KEY}`
-    );
-    return response.data.result;
-  } catch (error) {
-    console.error('Error fetching Gas price:', error);
-    throw error;
-  }
+export const getGasPrice = async () => {
+  return makeEtherscanRequest('gastracker', 'gasoracle');
 };
