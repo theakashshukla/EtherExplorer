58911c458fc2e667d54e4edccc4d475d45b12906
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-05 20:05:40 +0530

    Update margin in profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,7 @@ import { WalletInfo } from "@/components/Wallet-Info";
 
 export default function ProfilePage() {
   return (
-    <div className="m-5">
+    <div className="m-4">
       <WalletInfo />
     </div>
   );
